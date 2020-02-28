902fa5602cf56ebb8cfec111d445411213798d93
CaiusTheReaper <baijanmew@hotmail.com>
2020-02-28 18:45:03 +0000

    Cursor

---
+++ WIGJam/Assets/Cursory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursory : MonoBehaviour {
+
+    private SpriteRenderer rend;
+
+  
+
+    void Start(){
+        Cursor.visible = false;
+        rend = GetComponent<SpriteRenderer>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //
+        transform.position = cursorPos;
+        
+    }
+}
---
+++ WIGJam/Assets/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public GameObject card;
+    SpriteRenderer sr;
+    void Start()
+    {
+        sr = card.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
