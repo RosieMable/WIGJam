ad18651e71e4b7b3638aaffd6d828845306d25f1
RosieMable <sciarrillo.nella@gmail.com>
2020-02-29 16:33:18 +0000

    Mouse Cursor Fixed

---
+++ WIGJam/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MouseCursor : MonoBehaviour
+{
+
+    [SerializeField]
+    Canvas myCanvas;
+
+    void Start(){
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+        transform.position = myCanvas.transform.TransformPoint(pos);
+    }
+
+}
