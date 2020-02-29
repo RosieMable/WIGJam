8862691af84f73528685fedab9c5b637f9706bd5
CaiusTheReaper <baijanmew@hotmail.com>
2020-02-29 19:26:13 +0000

    1

--- WIGJam/Assets/GameLogic.cs
+++ WIGJam/Assets/GameLogic.cs
@@ -7,29 +7,38 @@ public class GameLogic : MonoBehaviour
 
     public GameObject card;
     public CardLogic cl;
-    SpriteRenderer sr;
+    [SerializeField] private Transform handPoint;
+    public Transform canvas;
+    Image sr;
     float fMovingSpeed = 1f;
     void Start()
     {
-        sr = card.GetComponent<SpriteRenderer>();
-
+        sr = card.GetComponent<Image>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0) && cl.isMouseOver)
+        if (Input.GetMouseButtonDown(0) && cl.isMouseOver)
         {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            card.transform.position = pos;
+            //Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //card.transform.position = pos;
+            if (card.transform.parent == canvas)
+            {
+                card.transform.SetParent(handPoint);
+            }
+            else
+            {
+                card.transform.SetParent(canvas);
+            }
         }
         else
         {
-            card.transform.position = Vector2.MoveTowards(card.transform.position, new Vector2(0, 0), fMovingSpeed);
+           //card.transform.position = Vector2.MoveTowards(card.transform.position, cl.originalPosition, fMovingSpeed);           
         }
         //checking the side
         if (card.transform.position.x > 2)
         {
-            sr.color = Color.green;
+            //sr.color = Color.green;
             if (!Input.GetMouseButton(0))
             {
                 cl.InduceRight();
@@ -37,7 +46,7 @@ public class GameLogic : MonoBehaviour
         }
         else if (card.transform.position.x < -2)
         {
-            sr.color = Color.red;
+            //sr.color = Color.red;
             if (!Input.GetMouseButton(0))
             {
                 cl.InduceLeft();
@@ -45,7 +54,7 @@ public class GameLogic : MonoBehaviour
         }
         else
         {
-            sr.color = Color.white;
+            //sr.color = Color.white;
         }
     }
 }
