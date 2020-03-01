640cd4cd1c2d13dc38ba840f745258d5ac27a47a
CaiusTheReaper <baijanmew@hotmail.com>
2020-03-01 12:10:25 +0000

    1

---
+++ WIGJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+
+public class MainMenu : MonoBehaviour
+{
+
+
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
+    public void LoadLevel(string levelname)
+    {
+        SceneManager.LoadScene(levelname);
+    }
+
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+
+    }
+}
