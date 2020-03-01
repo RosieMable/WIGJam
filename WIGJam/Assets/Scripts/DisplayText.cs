af93cac4e127074cd7ac9e1c06abf14e87fddba3
CaiusTheReaper <baijanmew@hotmail.com>
2020-03-01 16:39:09 +0000

    1

--- WIGJam/Assets/Scripts/DisplayText.cs
+++ WIGJam/Assets/Scripts/DisplayText.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class DisplayText : MonoBehaviour
 {
-    public string mystring;
+    public string myString;
     public Text myText;
     public float fadeTime;
     public bool displayInfo;
@@ -21,12 +21,19 @@ public class DisplayText : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        FadeText ()
-            if (Input.GetKeyDown (KeyCode.Escape))
+        FadeText();
+
+         /*   if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            Screen.lockCursor = false;
+        }
+        */
     }
-   void OnMouseOver()
+
+    void OnMouseOver()
     {
         displayInfo = true;
     }
@@ -36,18 +43,23 @@ public class DisplayText : MonoBehaviour
     {
         displayInfo = false;
     }
+
     void FadeText()
 
-        if(displayinfo)
     {
-        myText.text = myString;
-        myText.color = Color.Lerp (myText.color, Color.white, fadeTime * Time.deltaTime);
+        if (displayInfo)
+
+        {
+            myText.text = myString;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+
+        else
+        {
+
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+
+        }
     }
-
-    else
-{
-
-myText.color = Color.Lerp (myText.color, Color.clear, fadeTime * Time.deltaTime);
-
 }
 
